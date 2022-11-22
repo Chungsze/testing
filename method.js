489086555b97baaced96bfa7a778a299993c398e
Chungsze <ukrenthouse@gmail.com>
2022-11-22 17:13:44 +0000

    first commit

---
+++ method.js
@@ -0,0 +1,9 @@
+
+const blackFridayDeals = {
+    sony: 'Playstation 5',
+    discountPopup: function () {
+      console.log(`Get 50% off ${this.sony} only today!`)
+    },
+  }
+  
+  blackFridayDeals.discountPopup()
