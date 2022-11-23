3f91914daba7c40d5ae82670443d2c8be58365c4
Chungsze <ukrenthouse@gmail.com>
2022-11-23 23:08:03 +0000

    remove non-related items

--- method.js
+++
@@ -1,9 +0,0 @@
-
-const blackFridayDeals = {
-    sony: 'Playstation 5',
-    discountPopup: function () {
-      console.log(`Get 50% off ${this.sony} only today!`)
-    },
-  }
-  
-  blackFridayDeals.discountPopup()
